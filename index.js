51d661fde3f2b15381d9b38ae5c4be3fbcdff76b
ZA1TAMA <107215271+ZA1TAMA@users.noreply.github.com>
2024-07-13 14:48:18 -0400

    beta 2
    -added mobile navigation bar
    -fixed fonts (hopefully)

--- index.js
+++ index.js
@@ -4,5 +4,16 @@ function darkmode() {
 }
 
 function openTab(name) {
-    window.open(name, "_blank");
+    window.open(name, "_self");
+}
+const m = document.getElementById("mobilenav");
+m.style.height = "0px";
+
+function togglemenu() {
+    if (m.style.height == "0px") {
+        m.style.height = "15vh";
+        console.log("pkmn");
+    } else {
+        m.style.height = "0px";
+    }
 }
